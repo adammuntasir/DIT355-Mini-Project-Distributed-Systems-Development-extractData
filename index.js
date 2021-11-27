13b75fd6033d61109814adb6c49852a1d6ea0367
sramzi <sramzi@student.chalmers.se>
2021-11-27 09:07:20 +0100

    old changes

--- index.js
+++ index.js
@@ -8,15 +8,7 @@ publisher.start; //starts the publisher.js module
 
 subscriber.eventListener.on("mqttRecieved", function(topic, payload) {
 
-    console.log("the payload is " + (payload))
-    var messageExtracted = logic.extractData(payload)
-    if (messageExtracted == undefined) {
-        var readyToBePublished = JSON.stringify(payload)
 
-    } else {
-        console.log("the message is " + messageExtracted)
-            // var readyToBePublished = JSON.stringify(messageExtracted)
-    }
-    publisher.publish("ramzimkoqaz1984qwe4", messageExtracted)
+    publisher.publish("ramzimkoqaz1984qwe4", payload)
 
 })
